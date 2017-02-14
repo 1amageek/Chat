2eb31ae54d45102c3bd58802d28db7de342dd1b1
1_am_a_geek <tmy0x3@icloud.com>
2017-02-14 16:03:31 +0900

    add firebase

---
+++ Messenger/AppDelegate.swift
@@ -0,0 +1,36 @@
+//
+//  AppDelegate.swift
+//  Messenger
+//
+//  Created by 1amageek on 2017/02/14.
+//  Copyright © 2017年 Stamp inc. All rights reserved.
+//
+
+import UIKit
+import Firebase
+
+
+@UIApplicationMain
+class AppDelegate: UIResponder, UIApplicationDelegate {
+
+    var window: UIWindow?
+
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
+        
+        NSSetUncaughtExceptionHandler { exception in
+            debugPrint(exception.name)
+            debugPrint(exception.reason ?? "")
+            debugPrint(exception.callStackSymbols)
+        }
+        
+        FIRApp.configure()
+        
+        self.window = UIWindow(frame: UIScreen.main.bounds)
+        self.window?.rootViewController = UINavigationController(rootViewController: RoomViewController())
+        self.window?.makeKeyAndVisible()
+        
+        return true
+    }
+
+}
+
--- Podfile
+++ Podfile
@@ -3,24 +3,16 @@
 
 target 'Chat' do
   use_frameworks!
-
-  # Pods for Chat
   pod 'RealmSwift'
+end
 
+target 'Messenger' do
+  use_frameworks!
+  pod 'RealmSwift'
   pod 'Firebase'
   pod 'Firebase/Messaging'
   pod 'Firebase/Database'
   pod 'Firebase/Crash'
   pod 'Firebase/Auth'
   pod 'Firebase/Storage'
-
-end
-
-target 'TwitterMessenger' do
-  use_frameworks!
-
-  # Pods for Chat
-  pod 'RealmSwift'
-  pod 'APIKit', '~> 3.0'
-
 end
